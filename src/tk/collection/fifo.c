f2fc54bac42e5f31085bfd5aaf17a11f3330591e
Keidan (K. Billonneau) <kuso.fr@gmail.com>
2013-08-03 18:09:32 +0200

    add comments

--- src/tk/collection/fifo.c
+++ src/tk/collection/fifo.c
@@ -2,6 +2,11 @@
 #include <tk/sys/log.h>
 #include <stdlib.h>
 
+/**
+ * @fn fifo_t fifo_alloc()
+ * @brief Alloc new fifo.
+ * @return The fifo else NULL on error.
+ */
 fifo_t fifo_alloc() {
   fifo_t fifo = (fifo_t)malloc(sizeof(struct fifo_list_s));
   if(!fifo) {
@@ -13,6 +18,11 @@ fifo_t fifo_alloc() {
   return fifo;
 }
 
+/**
+ * @fn void fifo_free(fifo_t fifo)
+ * @brief Delete a fifo.
+ * @param fifo The fifo.
+ */
 void fifo_free(fifo_t fifo) {
   if (fifo && fifo->first) {
     fifo_clear(fifo);
@@ -20,6 +30,11 @@ void fifo_free(fifo_t fifo) {
   }
 }
 
+/**
+ * @fn void fifo_clear(fifo_t fifo)
+ * @brief Clear a fifo.
+ * @param fifo The fifo.
+ */
 void fifo_clear(fifo_t fifo) {
   struct fifo_element_s* p;
   if (fifo && fifo->first) {
@@ -30,18 +45,37 @@ void fifo_clear(fifo_t fifo) {
   }
 }
 
+/**
+ * @fn _Bool fifo_empty(fifo_t fifo)
+ * @brief Test if the fifo is empty.
+ * @param fifo The fifo.
+ * @return 1 if the fifo is empty else 0.
+ */
 _Bool fifo_empty(fifo_t fifo) {
   return fifo_size(fifo) == 0;
 }
 
-void fifo_foreach(fifo_t fifo, fifo_foreach_fct_t fct, void* userData) {
+/**
+ * @fn void fifo_foreach(fifo_t fifo, fifo_foreach_fct_t fct, void* user_data)
+ * @brief Iterate a fifo.
+ * @param fifo The fifo.
+ * @param fct Foreach callback.
+ * @param user_data A user data.
+ */
+void fifo_foreach(fifo_t fifo, fifo_foreach_fct_t fct, void* user_data) {
   struct fifo_element_s* p;
   if(fifo && fifo->first)
     for(p = fifo->first; p != fifo->last; p = p->next) {
-      fct(p->value, userData);
+      fct(p->value, user_data);
     }
 }
 
+/**
+ * @fn unsigned long fifo_size(fifo_t fifo)
+ * @brief Get the fifo size.
+ * @param fifo The fifo.
+ * @return The size.
+ */
 unsigned long fifo_size(fifo_t fifo) {
   struct fifo_element_s* p;
   unsigned long i = 0;
@@ -52,6 +86,12 @@ unsigned long fifo_size(fifo_t fifo) {
   return i;
 }
 
+/**
+ * @fn void fifo_push(fifo_t fifo, void* value)
+ * @brief Push a new value in the fifo.
+ * @param fifo The fifo.
+ * @param value The value.
+ */
 void fifo_push(fifo_t fifo, void* value) {
   if(!fifo) return;
   struct fifo_element_s* p = (struct fifo_element_s*)malloc(sizeof(struct fifo_element_s));
@@ -69,6 +109,12 @@ void fifo_push(fifo_t fifo, void* value) {
   p->next = NULL;
 }
 
+/**
+ * @fn void* fifo_pop(fifo_t fifo)
+ * @brief Pop a value from the fifo
+ * @param fifo The fifo.
+ * @return The value.
+ */
 void* fifo_pop(fifo_t fifo) {
   void * value = NULL;
   if(!fifo || !fifo->first) return NULL;
@@ -84,6 +130,13 @@ void* fifo_pop(fifo_t fifo) {
   return value;
 }
 
+/**
+ * @fn void* fifo_get(fifo_t fifo, long position)
+ * @brief Get a value from the fifo
+ * @param fifo The fifo.
+ * @param position The value position
+ * @return The value.
+ */
 void* fifo_get(fifo_t fifo, long position) {
   if(!fifo) return NULL;
   struct fifo_element_s* p;
