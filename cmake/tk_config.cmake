ade3b480ad29567f050c7bfc39c4f30c72c76a3f
Keidan (K. Billonneau) <kuso.fr@gmail.com>
2013-08-23 19:34:59 +0200

    change cfg check

---
+++ cmake/tk_config.cmake
@@ -0,0 +1,16 @@
+#check for config
+include(CheckIncludeFile)
+include(CheckTypeSize)
+
+check_include_file(zlib.h HAVE_ZLIB_H)
+check_include_file(termios.h HAVE_TERMIOS_H)
+check_include_file(ncurses.h HAVE_NCURSES_H)
+
+if(HAVE_ZLIB_H)
+  set(CMAKE_EXTRA_INCLUDE_FILES zconf.h)
+  check_type_size(z_crc_t HAVE_Z_CRC_T)
+  set(CMAKE_EXTRA_INCLUDE_FILES)
+  if(NOT HAVE_Z_CRC_T)
+    add_definitions("-Dz_crc_t=unsigned long")
+  endif(NOT HAVE_Z_CRC_T)
+endif(HAVE_ZLIB_H)
