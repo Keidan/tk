d89272c28da57b83d6beb196e4377c6a091e350b
Keidan (K. Billonneau) <kuso.fr@gmail.com>
2013-08-29 07:31:19 +0200

    fix code for the latese commit

--- src/test/test_z.c
+++ src/test/test_z.c
@@ -9,12 +9,12 @@
 int main(int argc, char** argv) {
   ssig_init(log_init_cast_user("test_z", LOG_PID|LOG_CONS|LOG_PERROR), NULL);
   z_t z = z_new();
-  fifo_t files = fifo_new();
+  fifo_t files = fifo_new(1);
   file_list_dir("plop", files);
     /*fifo_push(files, "plop/test_shell");
   fifo_push(files, "plop/test_stringbuffer");
   fifo_push(files, "plop/z/test_z");*/
-  int r = z_compress(z, "plop.zip", NULL/*"azerty"*/, Z_C_BETTER, 0, 0, files, 1);
+  int r = z_compress(z, "plop.zip", NULL/*"azerty"*/, Z_C_BETTER, 0, 0, files);
   printf("Compression state: %d\n", r);
   fifo_delete(files);
 
