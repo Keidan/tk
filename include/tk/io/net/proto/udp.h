9316aa86424a6fa0da3d173039114f711c81fe12
Keidan (K. Billonneau) <kuso.fr@gmail.com>
2013-10-11 17:49:23 +0200

    first add of UDP protocol

---
+++ include/tk/io/net/proto/udp.h
@@ -0,0 +1,41 @@
+/**
+*******************************************************************************
+* @file udp.h
+* @author Keidan
+* @date 11/10/2013
+* @par Project
+* tk
+*
+* @par Copyright
+* Copyright 2011-2013 Keidan, all right reserved
+*
+* This software is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY.
+*
+* Licence summary : 
+*    You can modify and redistribute the sources code and binaries.
+*    You can send me the bug-fix
+*
+* Term of the licence in in the file licence.txt.
+*
+*******************************************************************************
+*/
+#ifndef __ETH_H__
+  #define __ETH_H__
+
+  /**
+   * @fn udp_t udp_new()
+   * @brief Open an UDP connection.
+   * @return The UDP context else NULL on error.
+   */
+  udp_t udp_new();
+
+  /**
+   * @fn void udp_delete(udp_t udp)
+   * @brief release the udp context.
+   * @param udp The context.
+   */
+  void udp_delete(udp_t udp);
+
+
+#endif /* __ETH_H__ */
