19df986b4797330f1c54ab9654a873492d51183b
Keidan (K. Billonneau) <kuso.fr@gmail.com>
2014-05-24 15:18:35 +0200

    Add doc

--- src/tk/io/sr.c
+++ src/tk/io/sr.c
@@ -1,3 +1,25 @@
+/**
+ *******************************************************************************
+ * @file sr.c
+ * @author Keidan
+ * @date 03/01/2013
+ * @par Project
+ * tk
+ *
+ * @par Copyright
+ * Copyright 2011-2013 Keidan, all right reserved
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY.
+ *
+ * Licence summary : 
+ *    You can modify and redistribute the sources code and binaries.
+ *    You can send me the bug-fix
+ *
+ * Term of the licence in in the file licence.txt.
+ *
+ *******************************************************************************
+ */
 #include <tk/io/sr.h>
 #include <tk/sys/log.h>
 #include <sys/stat.h>
